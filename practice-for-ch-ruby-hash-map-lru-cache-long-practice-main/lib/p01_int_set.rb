58750369fa2420ab572ab59dcb4f692035f2d0b3
Dominic <domcashiola@gmail.com>
2023-11-13 14:26:04 -0500

    Completed part 1

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
@@ -1,19 +1,29 @@
 class MaxIntSet
+  attr_reader :store
   def initialize(max)
+    @max = max
+    @store = Array.new(max)
   end
 
   def insert(num)
+    
+    @store[num] = true if is_valid?(num)
+
   end
 
   def remove(num)
+    @store[num] = false if is_valid?(num)
   end
 
   def include?(num)
+    return true if @store[num] == true
   end
 
   private
 
   def is_valid?(num)
+    raise "Out of bounds" if num < 0 || num > @max
+    return true
   end
 
   def validate!(num)
@@ -26,18 +36,22 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -54,12 +68,24 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if @count >= num_buckets
+      resize!
+    end
+    unless include?(num)
+      self[num] << num
+      @count += 1
+    end
   end
 
   def remove(num)
+    if include?(num) 
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
@@ -69,9 +95,19 @@ class ResizingIntSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+
+
+    @store.each do |bucket|
+      bucket.each do |num|
+        new_store[num % (num_buckets * 2)] << num
+      end
+    end
+
+    @store = new_store
   end
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 end
